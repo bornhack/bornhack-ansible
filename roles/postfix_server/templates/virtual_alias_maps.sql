a2ec5b877f7ec0843d273207dd4b2fb96ba93c34
Thomas Steen Rasmussen <thomas@gibfest.dk>
2016-10-27 23:13:20 +0200

    update all submodules

--- roles/postfix_server/templates/virtual_alias_maps.sql
+++
@@ -1,5 +0,0 @@
-hosts={{ postgres_host }}
-user={{ postgres_user }}
-password={{ postgres_password }}
-dbname={{ postgres_dbname }}
-query = SELECT destination from virtual_alias_maps where lower(localpart||'@'||domain) = lower('%s')
--- roles/postfix_server/templates/virtual_mailbox_maps.sql
+++
@@ -1,5 +0,0 @@
-hosts={{ postgres_host }}
-user={{ postgres_user }}
-password={{ postgres_password }}
-dbname={{ postgres_dbname }}
-query=SELECT lower(localpart||'@'||domain) as username FROM users WHERE lower(localpart||'@'||domain) = lower('%s')
